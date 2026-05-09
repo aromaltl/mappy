625cb256cd7ed24a6960add155ee2830b7901007
aromaltl <103577905+aromaltl@users.noreply.github.com>
2026-05-09 16:02:35 +0530

    Update download URL for ExifTool in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget takeleap.in/ml-gui/Image-ExifTool-12.22.zip
+wget http://dashboard-images.seekright.com/SeekRight/DeploymentFiles/Exiftool.zip
 unzip Image-ExifTool-12.22.zip
 chmod -R 777 .
 c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) dirmap.cpp -o dirmap$(python3-config --extension-suffix)
